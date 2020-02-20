9835a21c7e60f2dfee021fb8960ebbba6d4a8923
James <james.craig@certara.com>
2020-02-19 17:30:28 -0800

    Replaced GROUP with STUDY for vignette stratification examples

--- doc/tidyvpc.R
+++ doc/tidyvpc.R
@@ -72,7 +72,7 @@ plot(vpc)
 
 vpc <- observed(obs_data, x=TIME, y=DV) %>%
     simulated(sim_data, y=DV) %>%
-    stratify(~ GENDER + GROUP) %>%
+    stratify(~ GENDER + STUDY) %>%
     binless() %>%
     vpcstats()
 
@@ -100,12 +100,12 @@ vpc <- observed(obs_data, x=TIME, y=DV) %>%
 plot(vpc)
 
 ## ----fig.width = 9, fig.height = 6, out.width=640, warning = FALSE-------
-obs_data$LLOQ <- obs_data[, ifelse(GENDER == "F", 100, 25)]
+obs_data$LLOQ <- obs_data[, ifelse(STUDY == "Study A", 50, 25)]
 
 vpc <- observed(obs_data, x=TIME, y=DV) %>%
     simulated(sim_data, y=DV) %>%
     censoring(blq=(DV < LLOQ), lloq=LLOQ) %>%
-    stratify(~ GENDER) %>%
+    stratify(~ STUDY) %>%
     binless() %>%
     vpcstats()
 
@@ -142,24 +142,24 @@ plot(vpc)
 ## ----fig.width = 9, fig.height = 6, out.width=640------------------------
 vpc <- observed(obs_data, x=TIME, y=DV) %>%
     simulated(sim_data, y=DV) %>%
-    stratify(~ GENDER + GROUP) %>%
+    stratify(~ GENDER + STUDY) %>%
     binning(stratum = list(GENDER = "M"), bin = "jenks", nbins = 7, by.strata = T) %>%
     binning(stratum = list(GENDER = "F"), bin = "centers", centers = c(1,3,5,7), by.strata = T) %>%
-    binning(stratum = list(GROUP = "Concomitant"), bin = "kmeans", by.strata = T) %>%
-    binning(stratum = list(GROUP = "No Concomitant"), bin = "pam", nbins = 10, by.strata = T) %>%
+    binning(stratum = list(STUDY = "Study A"), bin = "kmeans", by.strata = T) %>%
+    binning(stratum = list(STUDY = "Study B"), bin = "pam", nbins = 10, by.strata = T) %>%
     predcorrect(pred=PRED) %>%
     vpcstats()
 
 plot(vpc)
 
 ## ----warning = FALSE, fig.width = 9, fig.height = 6, out.width=640-------
-new_lambda = data.frame(GENDERM = c(1,3,.8), GENDERF = c(2,4,1))
+new_lambda = data.frame(GENDERF = c(2,4,2), GENDERM = c(1,3,.8) )
 
 vpc <- observed(obs_data, x=TIME, y=DV) %>%
     simulated(sim_data, y=DV) %>%
     stratify(~ GENDER) %>%
     predcorrect(pred=PRED) %>%
-    binless(optimize = FALSE, lambda = new_lambda, loess.ypc = TRUE, span = c(.43, .85)) %>%
+    binless(optimize = FALSE, lambda = new_lambda, loess.ypc = TRUE, span = c(.6, .85)) %>%
     vpcstats()
 
 plot(vpc)
@@ -167,10 +167,9 @@ plot(vpc)
 ## ----fig.width = 9, fig.height = 6, out.width=640, warning = FALSE-------
 vpc <- observed(obs_data, x=TIME, y=DV) %>%
     simulated(sim_data, y=DV) %>%
-    binning(bin = "jenks", nbins = 7) %>%
-    vpcstats()
+    binning(bin = "jenks", nbins = 7)
 
-plot(vpc, show.stats = FALSE)
+plot(vpc)
 
 
 ## ------------------------------------------------------------------------
@@ -195,23 +194,22 @@ ggplot(vpc$pctblq, aes(x=xbin, y=md))+
   ylab("%blq")
 
 ## ----fig.width = 9, fig.height = 6, out.width=640, warning=FALSE---------
-obs_data$LLOQ <- 50
+obs_data$LLOQ <- obs_data[, ifelse(STUDY == "Study A", 50, 25)]
 
 vpc <- observed(obs_data, x = TIME, y = DV) %>%
   simulated(sim_data, y = DV) %>%
   censoring(blq = DV < LLOQ, lloq = LLOQ) %>%
-  stratify(~GENDER) %>%
-  binning(bin = NTIME) %>%
+  stratify(~STUDY) %>%
+  binning(bin = "kmeans", nbins = 7) %>%
   vpcstats()
 
 ggplot(vpc$stats, aes(x = xbin)) + 
-  facet_grid(~GENDER, scales = "free", as.table = FALSE) + 
+  facet_grid(~STUDY, scales = "free", as.table = FALSE) + 
   geom_ribbon(aes(ymin = lo, ymax = hi, fill = qname, col = qname, group = qname),alpha = 0.1, col = NA) + 
   geom_line(aes(y = md, col = qname, group = qname)) +
   geom_line(aes(y = y, linetype = qname), size = 1) + 
-  geom_hline(data = unique(vpc$data[, .(LLOQ), by = eval("GENDER")]), 
-            aes(yintercept = !!as.symbol("LLOQ")), linetype = "dotted", size = 1) +
-  geom_text(data = unique(vpc$data[, .(LLOQ), by = eval("GENDER")]), 
+  geom_hline(data=unique(obs_data[, .(STUDY, LLOQ)]), aes(yintercept=LLOQ), linetype="dotted", size=1) +
+  geom_text(data = unique(vpc$data[, .(LLOQ), by = "STUDY"]), 
             aes(x = 10, y = LLOQ, label = paste("LLOQ", LLOQ, sep = "="), ), vjust = 1, hjust = 1) +
   scale_colour_manual(name = "Simulated Percentiles\nMedian (lines) 95% CI (areas)",
                     breaks = c("q0.05", "q0.5", "q0.95"), 
@@ -227,7 +225,7 @@ ggplot(vpc$stats, aes(x = xbin)) +
                     labels = c("5%", "50%", "95%")) + 
   guides(fill = guide_legend(order = 2), colour = guide_legend(order = 2), linetype = guide_legend(order = 1)) + 
   theme(legend.position = "top", legend.key.width = grid::unit(2, "cm")) + 
-  labs(x = "NTIME", y = "Concentration") + 
+  labs(x = "TIME", y = "Concentration") + 
   geom_point(data = vpc$obs, aes(x = x, y = y), size = 1, alpha = 0.1, show.legend = F) + 
   geom_vline(data = bininfo(vpc)[, .(x = sort(unique(c(xleft, xright)))), by = names(vpc$strat)],aes(xintercept = x), size = rel(0.5), col = "gray80") + 
   theme(panel.grid = element_blank()) + 
@@ -236,9 +234,9 @@ ggplot(vpc$stats, aes(x = xbin)) +
 
 ## ----fig.width = 9, fig.height = 6, out.width=640, warning = FALSE-------
 ggplot(vpc$pctblq) + 
-  facet_grid(~GENDER) +
+  facet_grid(~STUDY) +
   geom_ribbon(aes(x = xbin, ymin= lo, ymax = hi), fill = "red", alpha = .2) + 
   geom_line(aes(x = xbin, y = y)) + 
   geom_line(aes(x = xbin, y = md), color = "red") + 
-  labs(x= "NTIME", y= "% BLQ")
+  labs(x= "TIME", y= "% BLQ")
 
